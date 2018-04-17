774c3f5b90bf4fcbbafe6a09d9c840a5154efc0f
Abhishek Jaiswal <jaiswalabhishek543@gmail.com>
2018-04-17 11:38:12 +0530

    descpalansort done

--- src/Training/DescPalindromeSort.java
+++ src/Training/DescPalindromeSort.java
@@ -1,84 +1,52 @@
 package Training;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class DescPalindromeSort {
 	
+
+	public static void checkPalindrome(String s)
+	  {
+	    // reverse the given String
+	    String reverse = new StringBuffer(s).reverse().toString();
+	 
+	    // checking for palindrome
+	    if (s.equals(reverse))
+	    {  
+	    	
+	    		List<String> myList = new ArrayList<String>();
+	    		myList.add(reverse);
+	    		Collections.sort(myList, Collections.reverseOrder());
+	    		
+	    		
+	    		for(String str: myList){
+	    			System.out.println(str);
+	    	   }
+	    	
+	    	
+	    	      }
+	    	
+	    	
+	    	
+	    
+	    	System.out.println();
+	    }
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args)
 	{
 		
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter 10 inputs");
-		String[] arr=new String[10];
-		int[] intarr=new int[10];
-		int count=0;
-		String rev="";
-		for(int i=0;i<10;i++) 
-		{
-			arr[i]=sc.nextLine();   //Taking 10 inputs from user
-			
-			
-		}
 		
-		
-		//Checking for the palindrome in the inputs 
-		for(int j=0;j<10;j++)  
-		{
-			int len = arr[j].length();
-			 
-		      for ( int k = len - 1; k >= 0; k-- )
-		         rev = rev + arr[j].charAt(k);
-		 
-		      if (arr[j].equals(rev))
-		      {
-		    	  intarr[count]=j;
-		    	  count++;
-		      }
-	
-			
-		}
-		
-		
-		
-		
-		
-		String[] arr2=new String[count];
-		int n=0;
-		if(count>0)
-		{
-			for(int x=0;x<=count;x++)
-			{
-				arr2[n]=arr[intarr[x]];
-				n++;
-			}
-			
-			
-			
-			
-		}
-		
-		
-		//Sort the array of string
-		 for(int y=1;y<=count;y++)
-		{
-			String temp = arr2[y];
-
-			int z = y - 1;
-			while (z >= 0 && temp.length() < arr2[z].length())
-			{
-				arr2[z+1] = arr2[z];
-				z--;
-			}
-			arr2[z+1] = temp;
-		}
-		 
-		 
-			for (int m=0; m<=count; m++)
-				{System.out.println(arr2[m]);}
-		
-		
-		
-		
+		checkPalindrome("12321");
+	    checkPalindrome("121");
+	    
 		
 		
 	}
